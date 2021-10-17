15aa4d421eab0b86a8e7d76ebfee9d6fcc28accd
Adrián García Pérez <adrian.garcia@undostres.com.mx>
2021-10-17 18:51:37 -0500

    Improving UI

--- app/src/main/java/com/piggy/piggy/TransactionsExpandableListAdapter.kt
+++ app/src/main/java/com/piggy/piggy/TransactionsExpandableListAdapter.kt
@@ -1,11 +1,20 @@
 package com.piggy.piggy
 
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.text.NumberFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.util.TimeZone
+import java.time.format.DateTimeFormatter
 
 class TransactionsExpandableListAdapter(private var context: Context,
                                         private var months: List<String>,
@@ -41,33 +50,68 @@ class TransactionsExpandableListAdapter(private var context: Context,
     }
 
     override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
-        var convertView = convertView
+        var newView: View? = convertView
         val monthTitle = getGroup(groupPosition) as String
 
         if (convertView == null) {
             val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            convertView = inflater.inflate(R.layout.month_group, null)
+            newView = inflater.inflate(R.layout.month_group, null)
         }
 
-        val monthTitleTV = convertView!!.findViewById<TextView>(R.id.month_title)
+        val monthTitleTV = newView!!.findViewById<TextView>(R.id.month_title)
         monthTitleTV.text = monthTitle
 
-        return convertView
+        return newView
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getChildView(groupPosition: Int, childPosition: Int, isLastChild: Boolean, convertView: View?, parent: ViewGroup?): View {
-        var convertView = convertView
-        val transactionTitle = (getChild(groupPosition, childPosition) as Transaction).title
+        var newView = convertView
+        val transaction = getChild(groupPosition, childPosition) as Transaction
+        val transactionTitle  = transaction.title
+        val transactionAmount = transaction.amount
+        val transactionDate = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(transaction.createdAt()),
+            TimeZone.getDefault().toZoneId()
+        )
 
-        if (convertView == null) {
+        if (newView == null) {
             val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            convertView = inflater.inflate(R.layout.transaction_item, null)
+            newView = inflater.inflate(R.layout.transaction_item, null)
         }
 
-        val transactiontitleTV = convertView!!.findViewById<TextView>(R.id.transaction_title)
-        transactiontitleTV.text = transactionTitle
+        val transactionTitleTV  = newView!!.findViewById<TextView>(R.id.transaction_title)
+        val transactionAmountTV = newView.findViewById<TextView>(R.id.transaction_amount)
+        val transactionDateTV   = newView.findViewById<TextView>(R.id.transaction_date)
+        val transactionIconIV   = newView.findViewById<ImageView>(R.id.transaction_icon)
 
-        return convertView
+        val resource = when (transaction.category) {
+            TransactionCategory.CARD -> R.drawable.ic_card
+            TransactionCategory.CASH -> R.drawable.ic_cash
+            TransactionCategory.PTRANSPORT -> R.drawable.ic_ptransport
+            TransactionCategory.CAR -> R.drawable.ic_car
+        }
+
+        val formatter  = DateTimeFormatter.ofPattern("dd MMM, hh:mm a")
+        val date       = transactionDate.format(formatter)
+
+        transactionTitleTV.text  = transactionTitle
+        transactionDateTV.text   = date
+        transactionIconIV.setImageResource(resource)
+
+        val nf: NumberFormat = NumberFormat.getInstance()
+        val transactionAmountString = nf.format(transactionAmount)
+
+        if (transaction.type == TransactionType.INCOME) {
+            transactionAmountTV.text = context.getString(R.string.pos_amount_template, transactionAmountString)
+            transactionAmountTV.setTextColor(Color.parseColor("#FEC260"))
+        }
+        else {
+            transactionAmountTV.text = context.getString(R.string.neg_amount_template, transactionAmountString)
+            transactionAmountTV.setTextColor(Color.parseColor("#C4C4C4"))
+        }
+
+        return newView
     }
 
     override fun isChildSelectable(p0: Int, p1: Int): Boolean {
